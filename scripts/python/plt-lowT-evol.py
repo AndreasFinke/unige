b6e10b682dbe59d817d109f8a71e2d6d146dc072
AndreasFinke <andipotal+github@gmail.com>
2019-07-01 16:23:02 +0200

    fixed bug with the last script

--- scripts/python/plt-lowT-evol.py
+++ scripts/python/plt-lowT-evol.py
@@ -1,13 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from os import listdir 
 
-directory = '/rtMP'
+directory = '/Users/af/Work/Cosmo/unige/scripts/python/rtMP'
+
+def list_files(directory, extension):
+    return (f for f in listdir(directory) if f.endswith('.' + extension))
+
 filenames = list_files(directory, "txt")
+
 #filenames = ['rtMP/0.txt', 'rtMP/1.txt', 'rtMP/2.txt','rtMP/3.txt','rtMP/4.txt']
+
 likelihoods = [] #each entry a vector with the time evolution, one per file! 
 
 for filename in filenames:
-    fp = open(filename)
+    fp = open(directory + "/" + filename)
     lines = fp.readlines()
     data = [[float(v) for v in line.split()] for line in lines]
     data = np.array(data)
@@ -16,3 +23,5 @@ for filename in filenames:
 
 for lh in likelihoods:
     plt.plot(lh)
+
+plt.savefig("evol.pdf")
