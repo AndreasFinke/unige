5a0a1816fd9d0e32afb5a8c2178633fc4f1ee016
AndreasFinke <andipotal+github@gmail.com>
2019-07-01 16:04:48 +0200

    added simple plotting script for posterior evolution

---
+++ scripts/python/plt-lowT-evol.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+filenames = ['rtMP/0.txt', 'rtMP/1.txt', 'rtMP/2.txt','rtMP/3.txt','rtMP/4.txt']
+likelihoods = [] #each entry a vector with the time evolution, one per file! 
+
+for filename in filenames:
+    fp = open(filename)
+    lines = fp.readlines()
+    data = [[float(v) for v in line.split()] for line in lines]
+    data = np.array(data)
+    likelihoods.append(data[:,1])
+ 
+
+for lh in likelihoods:
+    plt.plot(lh)
